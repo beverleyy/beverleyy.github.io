363ebb7268d8802935942ede482c7f463f900a56
beverleyy <62889133+beverleyy@users.noreply.github.com>
2024-01-12 13:25:48 -0800

    Fix bad XMLHTTP request

--- js/buildnews.js
+++ js/buildnews.js
@@ -1,9 +1,12 @@
 /* For the CORS-Anywhere server, I'm just reusing the one I made for the MOE2018TRF005 study because too lazy to make a new one... */
-//var cors_api = 'https://cors-anywhere.herokuapp.com';
+// var cors_api = 'https://cors-anywhere.herokuapp.com';
 var cors_api = 'https://moe2018trf005cors.onrender.com';
 var spreadsheet = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vS9lrQ1g5CiWV7fgUbIa5SFHFAB50fu4Z8cZOsKITFb5pkFSOQTJHSSvU-4Aj3i6xbtS49f78NXNA36/pub?gid=0&single=true&output=csv';
+var spreadsheet2 = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSZWu5VQRpDExhcugQvKzTAne3V4eZnuBelxnHlC_Ziz--qfnAiKWEPA3DX4z1p9dNlFqKgGn079-ii/pub?gid=0&single=true&output=csv';
 
 var placeHolder = [];
+var presentations = [], 
+    articles = [];
 
 var cors_api_host = cors_api.replace('https://','');
 var cors_api_url = cors_api + '/';
@@ -58,3 +61,57 @@ x.onload = function() {
  	});
 };
 x.send();
+
+var x2 = new XMLHttpRequest();
+x2.open('GET', cors_api+'/'+spreadsheet2);
+x2.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+x2.onload = function() {
+	Papa.parse(x2.responseText,{
+ 		//download: true, 
+ 		header: true,
+ 		step: function(row) {
+ 			var thisRow = row.data;
+            var thisType = thisRow.Type;
+            var newLi = document.createElement("li");
+            var contents = thisRow.Citation;
+            contents = contents.replaceAll('Yeo K.W.B.','<em>Yeo K.W.B.</em>').replaceAll('K.W.B. Yeo','<em>K.W.B. Yeo</em>')
+            if (thisRow.DOI){
+                contents += ' ' //add empty space
+                contents += 'doi: <a href="https://doi.org/'+thisRow.DOI+'">'+thisRow.DOI+'</a>'
+            }
+            newLi.innerHTML = contents;
+            newLi.classList.add("ti-caret-right")
+            if (thisType.toLowerCase() === 'presentation') presentations.push(newLi)
+            else if (thisType.toLowerCase() === 'article') articles.push(newLi)
+ 		},
+ 		complete: function (results) {
+			var container = document.querySelectorAll("#pubslist .terminal")[0];
+			container.innerHTML = "";
+            articles = articles.reverse()
+            presentations = presentations.reverse()
+            if(articles.length){
+                var newText = document.createElement("p");
+                newText.classList.add("cmd");
+                newText.innerHTML = "<strong>publications@beverleyy:~$</strong> ls journal_papers";
+                container.appendChild(newText)
+                var newList = document.createElement("ul")
+                articles.forEach((item) => {
+                    newList.appendChild(item)
+                })
+                container.appendChild(newList)
+            }
+            if(presentations.length){
+                var newText = document.createElement("p");
+                newText.classList.add("cmd");
+                newText.innerHTML = "<strong>publications@beverleyy:~$</strong> ls conference_presentations";
+                container.appendChild(newText)
+                var newList = document.createElement("ul")
+                presentations.forEach((item) => {
+                    newList.appendChild(item)
+                })
+                container.appendChild(newList)
+            }
+ 		}
+ 	});
+};
+x2.send();
