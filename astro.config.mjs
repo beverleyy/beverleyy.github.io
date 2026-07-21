46cd4248d58868bd1485e67557c1605d81cb1648
Beverley <62889133+beverleyy@users.noreply.github.com>
2026-07-20 23:22:55 -0600

    Touchup and fix some bugs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://example.edu',
-});
+  site: 'https://beverleyy.github.io',
+})
